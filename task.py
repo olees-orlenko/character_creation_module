58ea458174eb24c227e0c5d4244539c5cd44c3fb
Olesya Orlenko <olees.orlenko@yandex.ru>
2022-12-15 16:38:00 +0300

    Add docstring

--- task.py
+++ task.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 
 def attack(char_name: str, char_class: str) -> Optional[str]:
-    """Вычисляет урон, нанесенный противнику."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику, равный '
                 f'{5 + randint(3, 5)}')
@@ -17,7 +16,6 @@ def attack(char_name: str, char_class: str) -> Optional[str]:
 
 
 def defence(char_name: str, char_class: str) -> Optional[str]:
-    """Вычисляет заблокированные удары."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -28,7 +26,6 @@ def defence(char_name: str, char_class: str) -> Optional[str]:
 
 
 def special(char_name: str, char_class: str) -> str:
-    """Описание специальных умений персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость{80+25}»')
@@ -40,7 +37,6 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-    """Тренировка навыков персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,7 +59,6 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> Optional[str]:
-    """Выбор и характеристика персонажа."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -79,6 +74,8 @@ def choice_char_class() -> Optional[str]:
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
 
+
+def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
